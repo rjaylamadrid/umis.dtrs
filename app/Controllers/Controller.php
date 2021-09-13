43e02b2398cdd73391590bd2f5377646372bbdee
wilson0611 <60420561+wilson0611@users.noreply.github.com>
2021-09-13 09:09:27 +0800

    changes

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -14,8 +14,7 @@ class Controller {
         $this->user = $_SESSION['user'];
         $this->view = new SmartyView ();
         $this->data = $_POST;
-        $this->access(); 
-        // print_r($this->user);
+        $this->access();
         $this->view->assign (["user" => $this->user, "page" => $this->request_page ()]);
     }
 
@@ -31,20 +30,13 @@ class Controller {
     protected function access () {
         $str_access = 'S!mU-ABn8bC*3';
         $headers = array(["url" => "dashboard", "icon" => "home", "title" => "Dashboard"], ["url" => "employees", "icon" => "users", "title" => "Employees"], ["url" => "payroll", "icon" => "credit-card", "title" => "Payroll"], ["url" => "attendance", "icon" => "clock", "title" => "Daily Time Record"], ["url" => "calendar", "icon" => "calendar", "title" => "Calendar"],["url" => "leave", "icon" => "mail", "title" => "Leave Request"],["url" => "settings", "icon" => "settings", "title" => "Settings"], ["url" => "profile", "icon" => "user", "title" => "Profile"], ["url" => "payslip", "icon" => "credit-card", "title" => "Payroll"], ["url" => "dtr", "icon" => "clock", "title" => "Daily Time Record"], ["url" => "leave", "icon" => "file", "title" => "Leave Management"]);
-       
-        // $str_access = 'S!m-A8bC*3I';
-        // $headers = array(["url" => "dashboard", "icon" => "home", "title" => "Dashboard"], ["url" => "employees", "icon" => "users", "title" => "Employees"], ["url" => "attendance", "icon" => "clock", "title" => "Daily Time Record"], ["url" => "calendar", "icon" => "calendar", "title" => "Calendar"], ["url" => "settings", "icon" => "settings", "title" => "Settings"], ["url" => "profile", "icon" => "user", "title" => "Profile"], ["url" => "dtr", "icon" => "clock", "title" => "Daily Time Record"]);
         $u_headers = [];
       
-      
-       $access = $this->user['is_admin'] ? $this->user['access'] : $this->user['user_access'];
+        $access = $this->user['is_admin'] ? $this->user['access'] : $this->user['user_access'];
        
         for($i=0; $i<strlen($access); $i++) {
             $u_headers[] = $headers[strpos($str_access,substr($access, $i, 1),0)];
-           
-         
         }
-      
         $this->view->assign(["headers" => $u_headers]);
     }
 }
